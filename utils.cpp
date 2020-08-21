a27cf9053f31bcc49ca078e54ee5f88969a8ae38
Hugo Brunie <hbrunie0@gmail.com>
2020-08-20 17:10:31 -0700

    Generate orthogonal matrix.

--- utils.cpp
+++ utils.cpp
@@ -7,8 +7,21 @@
  *
  * Matrix D heap memory management must be handle outside the function.
  * D is a square matrix of size n by n.
- * */
+ */
 void generate_diagonal_matrix(DATATYPE * D, int n, DATATYPE cond){
+    // Reset to 0 values inside D
+    memset(D,0,n*n*sizeof(DATATYPE));
+    // Fill D with diagonal values
     for(int i=0; i<n; i++)
         D[n*i+i] = (DATATYPE)1. - (i-(DATATYPE)1.)(n-(DATATYPE)1.)*(1-1/(1-cond));
+}
+
+/* Matrix Q heap memory management must be handle outside the function.
+ * Q is a square matrix of size n by n.
+ */
+void generate_orthogonal_matrix(DATATYPE * Q, int n){
+    // Reset to 0 values inside Q
+    memset(D,0,n*n*sizeof(DATATYPE));
+    generate_random_matrix(Q,n);
+    orthogonalize_matrix(Q,n)
 }
